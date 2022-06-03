6d7478993c889b1f421f9ae3a8d6a12b0543e179
Esat <mesataksoy@gmail.com>
2022-06-03 14:57:03 +0200

    Jinja.py modified.

--- Flask02/jinja.py
+++ Flask02/jinja.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def head():
-    return render_template('index.html', number1 = 7000, number2 = 9000)
+    return render_template('index.html', number1 = 50, number2 = 20)
 
 @app.route('/mult')
 def number():
-    var1, var2 = 3041, 7060
+    var1, var2 = 60, 15
     return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
 
 if __name__ == '__main__':
